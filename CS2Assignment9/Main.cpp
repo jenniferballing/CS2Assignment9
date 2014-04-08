6c4986419b79e5b640242d7b9d920577db11cd17
Jen <jenniferballing@gmail.com>
2014-04-08 07:51:40 -0600

    push and pop are working

--- CS2Assignment9/Main.cpp
+++ CS2Assignment9/Main.cpp
@@ -3,78 +3,79 @@
 #include "TUStack.h"
 
 using namespace std;
-int counter;
-int arr [1];
-
-template <class T>
-TUStack<T>::TUStack()
-{
-    size = 0;
-    position = NULL;
-}
-template <class T>
-TUStack<T>::TUStack( int nSize)
-{
-    size = nSize;
-    position = new T [nSize];    
-}
-template <class T>
-void TUStack <T>::Push(T i)
-{
-    arr[counter]=i;
-    counter++;
-}
-template <class T>
-TUStack<T>::TUStack(const TUStack & obj)
-{
-    size = obj.size;
-    position = new T[size];
-    for( int i=0; i<size; i++)
-    {
-        position[i]= obj.position[i];
-    }
-}
-template < class T >
-T TUStack<T>::Pop()
-{
-    int value = arr[counter-1];
-    arr[counter-1]=NULL;
-    return value;
-}
-template <class T>
-int TUStack<T>::Size()
-{
-    return size;
-}
-template< class T>
-int TUStack<T>::Position()
-{
-    return position;
-}
-template <class T>
-T& TUStack <T> ::operator[] (int i)
-{
-    return position[i];
-}
-template <class T>
-TUStack<T>::~TUStack(void)
-{
-    delete [] position;
-}
-
-
+//int counter;
+char *arr [10];
+//string sArr[10];
 int main ()
 {
-    counter =0;
+    //int counter = 0;
+       
+    //Initialize
+    cout<<"Initialize the stack with ints: ";
+    cout<<"Initialize the stack with strings: ";
     stack<int> intStack;
-    TUStack<int> stacky (10);
-   
-    intStack.push(123);
-    stacky.Push(123);
+    stack<string> stringStack;
 
-    cout<<"intStack: "<<intStack.top()<<endl;
-    intStack.pop();
-    cout<<"stacky: "<<stacky.Pop()<<endl;
+    TUStack<int> TStackInt (10);
+    TUStack<string> TStackString(10);
+   
+    //push
+    cout<<"Push ints: 123, 560, 400, 890"<<endl;
+    cout<<"Push strings: Hello World, Hi Mom!"<<endl;
+    intStack.push(123);
+    TStackInt.Push(123);
+    intStack.push(560);
+    TStackInt.Push(560);
+    intStack.push(400);
+    TStackInt.Push(400);
+    intStack.push(890);
+    TStackInt.Push(890);
+
+
+    stringStack.push("HelloWorld");
+    TStackString.Push("HelloWorld");
+    stringStack.push("HiMom!");
+    TStackString.Push("HiMom!");
+
+    //Pop
+    cout<<"Pop the last number (890): "<<TStackInt.Pop()<<endl;
+    cout<<"Pop the last number (890): "<<intStack.top()<<endl;
+     intStack.pop();
+
+    //copy constructor
+    cout<<"Copy Constructor: "<<endl;
+
+    cout<<"Original int stack: "<<endl;
+    for(int j=0; j<sizeof(TStackInt)-1; j++)
+    {
+        cout<<TStackInt[j]<<endl;
+    }
+    cout<<"Original string stack: "<<endl;
+    for( int j=0; j<sizeof(TStackString)-1; j++)
+    {
+        cout<<TStackString[j]<<endl;
+    }
+    
+    TUStack <int> newTu(TStackInt);
+    TUStack<string> newTuString(TStackString);
+    cout<<"Copied int stack: "<<endl;
+    for(int i=0; i<sizeof(newTu)-1; i++)
+    {
+        cout<< newTu[i]<<endl;
+    }
+    cout<<"Copied string stack: "<<endl;
+    for(int i=0; i<sizeof(newTuString)-1; i++)
+    {
+        cout<< newTuString[i]<<endl;
+    }
+    /*stack<int> newStack;
+    newStack=intStack;
+    cout<<"Test case: "<<endl;
+    for(int k=0; k<sizeof(newStack); k++)
+    {
+        cout<<newStack[k]<<endl;
+    }*/
+    
      
     return 0;
 }
--- CS2Assignment9/TUStack.h
+++ CS2Assignment9/TUStack.h
@@ -4,18 +4,20 @@
 #include <string>
 #include<stack>
 
+using namespace std;
+
 template <class T>
 class TUStack
 {
 private:
     int size;
-    T * position;
+    T * arr;
 
 public:
     TUStack(void);
     TUStack(int nSize);
 
-    TUStack(const TUStack & obj);
+    TUStack(const TUStack &obj);
 
     void Push(T item);//TUStack item);
     T Pop();
@@ -23,6 +25,99 @@ public:
     int Position();
     T& operator[] (int);
     ~TUStack(void);
+
+class OutOfBounds
+{
+public:
+    public:
+    int index;
+    int size;
+    string message;
+
+    OutOfBounds (int i, int s, string m)
+    {
+        index = i;
+        size = s;
+        message = m;
+    }
 };
+};
+//CONSTRUCTORS
+template <class T>
+TUStack<T>::TUStack()
+{
+    size = 0;
+    arr = NULL;
+}
+template <class T>
+TUStack<T>::TUStack( int nSize)
+{
+    size = nSize;
+    arr = new T [nSize];    
+}
+
+//COPY CONSTRUCTOR
+template <class T>
+TUStack<T>::TUStack(const TUStack & obj)
+{
+    size = obj.size;
+    arr = new T[size];
+    for( int i=0; i<size; i++)
+    {
+        arr[i]= obj.arr[i];
+    }
+}
+
+//PUSH AND POP
+template <class T>
+void TUStack <T>::Push(T i)
+{
+    int arrSize = sizeof(arr);
+    arr[arrSize+1] = i;
+}
+template < class T >
+T TUStack<T>::Pop()
+{
+    int arrSize = sizeof(arr);
+    int value = arr[arrSize-1];
+    arr[arrSize-1]=NULL;
+    return value;
+}
+
+//SIZE AND POSITION
+template <class T>
+int TUStack<T>::Size()
+{
+    return size;
+}
+template< class T>
+int TUStack<T>::Position()
+{
+    return arr;
+}
+template <class T>
+
+//OVERLOADED OPERATOR
+T& TUStack <T> ::operator[] (int i)
+{
+    if( i < 0) throw OutOfBounds (i, size, "Index is negative");
+    else if (i >= size) throw OutOfBounds (i, size, "Index is bigger than array's size");
+    else return arr [i];
+}
+
+//DESTRUCTOR
+template <class T>
+TUStack<T>::~TUStack(void)
+{
+    delete [] arr;
+}
+
+//ERROR CHECKING
+void OutOfBounds ( int i, int size, string m)
+{
+    cout<< "Index: "<<i<<endl;
+    cout<< "Size: "<<size<<endl;
+    cout<< "Error message: "<<m<<endl;
+}
 #endif
 
